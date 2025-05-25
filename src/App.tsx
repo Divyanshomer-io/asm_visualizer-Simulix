29c7c3cdf772eeffbc6b54b5cd52c56cc0420276
Divyanshu Lila <120311240+Divyanshomer-io@users.noreply.github.com>
2025-05-25 13:28:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import AliasMethod from "./pages/AliasMethod";
 import Bootstrapping from "./pages/Bootstrapping";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
