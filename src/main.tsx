d907cb548f42d8bd1c9a9912327ca8c48f017c55
Divyanshu Lila <120311240+Divyanshomer-io@users.noreply.github.com>
2025-06-02 23:13:18 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react'; // <-- Add this import
 
 createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+    <Analytics /> {/* <-- Add this line */}
+  </React.StrictMode>
+);
