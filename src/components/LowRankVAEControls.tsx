a8008706269612cf39f384bc600df48b0f5bac84
Divyanshu Lila <120311240+Divyanshomer-io@users.noreply.github.com>
2025-06-06 20:09:54 +0530

    Update LowRankVAEControls.tsx

--- src/components/LowRankVAEControls.tsx
+++ src/components/LowRankVAEControls.tsx
@@ -84,7 +84,7 @@ const LowRankVAEControls: React.FC<LowRankVAEControlsProps> = ({
             <Label className="text-sm font-medium">
               Latent Dimension: {params.latentDim}
             </Label>
-            <InfoTooltip {...VAETooltips.latentDimension} side="right" maxwidth="150"/>
+            <InfoTooltip {...VAETooltips.latentDimension} side="right" maxwidth="50"/>
           </div>
           <Slider
             value={[params.latentDim]}
